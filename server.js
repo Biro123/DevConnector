abec2826a316fe15800f1dc77bd8627400cf75e0
unknown <Biro123@ntlworld.com>
2020-03-24 00:21:00 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// Set up an endpoint to return text to browser
+app.get('/', (req, res) => res.send('API Running'));
+
+// Set port from cloud if present - OR 5000 locally
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port' + PORT));
