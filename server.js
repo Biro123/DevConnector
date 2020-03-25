729985f29787e523c40a431195f3145608bb5678
unknown <Biro123@ntlworld.com>
2020-03-25 00:17:01 +0000

    Initial setup of routes

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ connectDB();
 // Set up an endpoint to return text to browser
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 // Set port from cloud if present - OR 5000 locally
 const PORT = process.env.PORT || 5000;
 
