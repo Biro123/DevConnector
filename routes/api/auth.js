f57c1316903a57cda25079076f3c44e912a9b8ae
unknown <Biro123@ntlworld.com>
2020-03-26 01:07:38 +0000

    Creat auth middleware for protected routs

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test rout
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    // find the user in the database with the id from the token
+    // as decoded in auth middleware. Exclude password from db read
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
