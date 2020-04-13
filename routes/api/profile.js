4121359e1953ce499b7a66bb61993dfeb0e6cab0
unknown <Biro123@ntlworld.com>
2020-04-13 17:57:18 +0100

    Create a Delete Experience endpoint on the Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -220,4 +220,31 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    // @todo check that we have found the id provided before deleting
+    //    as it could remove the wrong one
+
+    // remove selected index from array
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
