8b2eeb237d518d5b2df8037927024aa054b1881c
unknown <Biro123@ntlworld.com>
2020-05-11 23:38:39 +0100

    New Alert cmpnt wired to Redux action and reducer

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,12 +1,15 @@
 // Redux Action is scalled from the component
 // and calls the reducer to update the State.
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid.v4();
+export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
+  const id = uuid();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  // Remove alert after 5 seconds
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
