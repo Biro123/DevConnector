510ec57b295067f930cf0eec03dc079c91e0ca3e
unknown <Biro123@ntlworld.com>
2020-05-11 12:11:46 +0100

    Set up Redux Action and Reducer for Alerts

---
+++ client/src/actions/alert.js
@@ -0,0 +1,12 @@
+// Redux Action is scalled from the component
+// and calls the reducer to update the State.
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType) => (dispatch) => {
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id }
+  });
+};
